5dfa01038264faab4d270a6b9b3312daf774bdef
Zhaniya110 <zmeyramova@gmail.com>
2024-05-12 18:37:17 +0500

    new + btn

---
+++ app.js
@@ -0,0 +1,10 @@
+var mySwiper = new Swiper('.swiper-container', {
+    freeMode: true,
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    scrollbar: {
+      el: '.js-swiper-scrollbar',
+      draggable: true,
+      snapOnRelease: false
+    }
+  });
